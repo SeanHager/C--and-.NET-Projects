aa9b9cc7fe5517923f0d46df02072933c5af9880
Sean M. Hager <smhager87@gmail.com>
2021-08-12 20:54:09 -0400

    Add files via upload

---
+++ Basic_C#_Programs/ComparisonProgram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program begin
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("\nTo get started, please hit the space key.\n");
+            Console.ReadKey();
+
+
+
+            // Prompt Person 1 and Person 2 input for hourly rate and hours worked.
+            Console.WriteLine("\nPlease enter in Person 1 hourly rate.\n");
+            int hourlyRatePersonOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter in Person 1 hours worked in week.\n");
+            int hoursWorkedPersonOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter in Person 2 hourly rate.\n");
+            int hourlyRatePersonTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter in Person 2 hours worked.\n");
+            int hoursWorkedPersonTwo = Convert.ToInt32(Console.ReadLine());
+
+
+            // Display current yearly salary based off of 52 week work year, based off of user previous input for Person 1 and 2.
+            Console.WriteLine("\nAnnual Salary of Person 1: Based on 52 week year.\n");
+            int totalSalaryPersonOne = hourlyRatePersonOne * hoursWorkedPersonOne * 52;
+            Console.WriteLine(totalSalaryPersonOne);
+
+            Console.WriteLine("\nAnnual Salary of Person 2: Based on 52 week year.\n");
+            int totalSalaryPersonTwo = hourlyRatePersonTwo * hoursWorkedPersonTwo * 52;
+            Console.WriteLine(totalSalaryPersonTwo);
+
+            // Compare total salary of Person 1 and 2 and boolean determine if Person 1 is > 2
+            Console.WriteLine("\nDoes Person 1 make more money than Person 2?\n");
+            bool incomeComparison = totalSalaryPersonOne > totalSalaryPersonTwo;
+            Console.WriteLine(incomeComparison);
+
+            // Program end
+            Console.WriteLine("\nThank you for using our program. Press enter to quit.\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/DailyReport/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console App Intro Statements
+            Console.WriteLine("The Tech Academy\n");
+            Console.WriteLine("Student Daily Report\n");
+
+            //Prompt user for input, assigned string "name" to Console.Readline for user input
+            Console.WriteLine("\nWhat is your name?\n");
+            string name = Console.ReadLine();
+
+            //Prompt user for input, assigned string "courseName" to Console.Readline for user input
+            Console.WriteLine("\nWhat course are you on?\n");
+            string courseName = Console.ReadLine();
+
+            //Prompt user for integer input, casting integer to Readline string for user input
+            Console.WriteLine("\nWhat page number?\n");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Prompt user for boolean input, converting string to boolean for user input
+            Console.WriteLine("\nDo you need help with anything? Please answer “true\" or \"false\"! \n");
+            bool courseHelp = Convert.ToBoolean(Console.ReadLine());
+
+            //Prompt user for input, assigned string "courseExperience" to Console.Readline for user input
+            Console.WriteLine("\nWere there any positive experiences you’d like to share? Please give specifics.\n");
+            string courseExperience = Console.ReadLine();
+
+            //Prompt user for input, assigned string "courseFeedback" to Console.Readline for user input
+            Console.WriteLine("\nIs there any other feedback you’d like to provide? Please be specific.\n");
+            string courseFeedback = Console.ReadLine();
+
+            //Prompt user for integer input, casting integer to Readline string for user input
+            Console.WriteLine("\nHow many hours did you study today?\n");
+            int studyTime = Convert.ToInt32(Console.ReadLine());
+
+            //Program end with Console.Writeline command and Console.Readline command to keep program open at end.
+            Console.WriteLine("\n\"Thank you for your answers. An Instructor will respond to this shortly. Have a great day!\" This is the end of the program.");
+
+            Console.ReadLine();
+        }
+    }
+}
