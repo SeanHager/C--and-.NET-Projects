c0960956890ba906b77956c4a0663f237df0fae2
Sean M. Hager <smhager87@gmail.com>
2021-08-21 21:11:36 -0400

    Add files via upload

---
+++ Basic_C#_Programs/CallingMethodsProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            mathoperation m = new mathoperation(); //created object to call methods in mathoperation class
+            Console.WriteLine("Please enter a number to do a simple formula on itself.");
+            int userInput = Convert.ToInt32(Console.ReadLine()); //user input
+
+            int answer1 = m.Add(userInput); //calling method Add
+            int answer2 = m.Subtract(userInput); //calling method Subtract
+            int answer3 = m.Multiply(userInput); //caling method Multiply
+
+            Console.WriteLine("This is your input + " + answer1);
+            Console.WriteLine("This is your input - " + answer2);
+            Console.WriteLine("This is your input * " + answer3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/CallingMethodsProject/mathoperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsProject
+{
+    public class mathoperation //class mathoperation
+    {
+        public int Add(int num)   //method Add
+        {
+            int result = num + num;
+            return (result);
+        }
+
+        public int Subtract(int num)   //method Subtract
+        {
+            int result = num - num;
+            return (result);
+        }
+
+        public int Multiply(int num)   //method Multiply
+        {
+            int result = num * num;
+            return (result);
+        }
+    }
+}
