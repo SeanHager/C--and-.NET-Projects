96555317f6497e8aa95758c247fed50fa24c2b5d
Sean M. Hager <smhager87@gmail.com>
2021-08-24 20:31:13 -0400

    Add files via upload

---
+++ Basic_C#_Programs/MainMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathoperations m = new mathoperations();    //create object to instantiate the class and call on methods
+
+            Console.WriteLine("Please enter in your first number.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());   //user enters number
+
+            Console.WriteLine("What is your second number? This number is optional. To bypass enter in no"); //second number optional
+            string userInput2 = Console.ReadLine();
+
+            if (userInput2 == "no")  //if user types no, method used will return whatever number they entered first + 10
+            {
+                Console.WriteLine(m.Add(userInput1));
+            }
+
+            else
+            {
+                int userInputMod = Convert.ToInt32(userInput2); //create variable userInpuitMod to convert string to int. if user enters in number, it will be added to the first
+                Console.WriteLine(m.Add(userInput1, userInputMod));
+            }
+
+            Console.ReadLine();             
+            int answer = m.mathOp(20);                  //addition method
+            Console.WriteLine(answer);
+
+            int answer2 = m.mathOp(2.2);                //decimal and subtraction method
+            Console.WriteLine(answer2);
+
+            int answer3 = m.mathOp("20");               //string and multiplication method
+            Console.WriteLine(answer3);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethod/mathoperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class mathoperations //created class
+    {
+        public int Add(int num1, int num2 = 10) //two parameters, one optional that adds 10 regardless to user input 1
+        {
+            int result = num1 + num2;
+            return result;                      //returns integer
+        }
+
+        public int mathOp(int num)                   //method mathOp()
+        {
+            int result = num + num;                  //takes integer and does addition 
+            return result;                           //returns answer as integer 
+
+        }
+        public int mathOp(double dec)                //method mathOp()
+        {
+            int result = Convert.ToInt32(dec - dec); //takes decimal and does subtraction
+            return result;                           //returns answer as integer
+        }
+
+        public int mathOp(string strNum)            //method mathOp()
+        {
+            int num = Convert.ToInt32(strNum);      //takes string and converts to int
+            int result = num * num;                 //does multiplication
+            return result;                          //returns answer as integer
+        }
+    }
+}
