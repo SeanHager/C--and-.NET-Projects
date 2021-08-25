9c2b0dac0511719b637e05a9bb7add0979433347
Sean M. Hager <smhager87@gmail.com>
2021-08-24 21:15:10 -0400

    Add files via upload

---
+++ Basic_C#_Programs/MethodProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodsoperation m = new methodsoperation(); // instantiate class methodsoperation
+
+            Console.WriteLine("Please enter in your first number.");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());   //user enters number
+
+            Console.WriteLine("What is your second number? This number is optional. Please type \"no\" if you would not like to enter a number."); //second number optional
+            string userInput2 = Console.ReadLine();
+
+            if (userInput2 == "no")  //if user types no, method used will return whatever number they entered first plus a standard programmed addition of 10 to first input
+            {
+                Console.WriteLine(m.Add(userInput1));
+            }
+
+            else
+            {
+                int userInputMod = Convert.ToInt32(userInput2); //create variable userInputMod to convert string to int. if user enters in number, it will be added to the first
+                Console.WriteLine(m.Add(userInput1, userInputMod));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodProject/methodsoperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodProject
+{
+    class methodsoperation
+    {
+        public int Add(int num1, int num2 = 10) //two parameters, one optional that adds 10 regardless to user input 1
+        {
+            int result = num1 + num2;
+            return result;                      //returns integer
+        }
+
+    }
+}
