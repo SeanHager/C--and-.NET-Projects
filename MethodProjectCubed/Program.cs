3d0537dea47941fbcf9b19411e35ac265040d218
Sean M. Hager <smhager87@gmail.com>
2021-08-25 20:03:30 -0400

    Add files via upload

---
+++ MethodProjectCubed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodProjectCubed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            mathoperations m = new mathoperations();    //create object to instantiate the class and call on methods
+
+            m.MultiplyCubed(2, 6);                      //math will be performed on first number, second number will be displayed
+            
+            m.MultiplyCubed(num: 3, num2: 20);          //calling both parameters
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
