675709c2ea2b778579db8db8165394a04cd70c93
Sean M. Hager <smhager87@gmail.com>
2021-08-27 19:54:03 -0400

    Add files via upload

---
+++ Basic_C#_Programs/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee();               //Instantiate Employee class
+            e.firstName = "Sample";                    //Instantied Employee Class has properties assigned to e
+            e.lastName = "Student"; 
+            e.SayName();                               //Call the SayName() method on the object
+            Console.ReadLine();
+        }
+    }
+}
