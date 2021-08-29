fb31e4046d2c838b51fe5efd21ddde960027d06c
Sean M. Hager <smhager87@gmail.com>
2021-08-28 20:02:03 -0400

    Add files via upload

---
+++ Basic_C#_Programs/Operators/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Employee
+    {
+        public int Id { get; set; } //property called Id and have it be of data type int
+
+
+        public static bool operator ==(Employee employee1, Employee employee2)  //comparing two objects w/ == operator 
+        {
+            return employee1.Id == employee2.Id;
+
+            bool same = false; //default
+
+            if (employee1.Id == employee2.Id) //overloading operator compares Employee objects by their ID properties
+            {
+                same = true; //return true if same
+            }
+
+            return same;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2) //comparing two objects w/ != operator, needed when utilizing operator == overload
+        {
+
+            return employee1.Id == employee2.Id;
+
+            bool same = true; //default
+
+            if (employee1.Id != employee2.Id) //overloading operator compares Employee objects by their ID properties
+            {
+                same = false;
+            }
+
+            return same;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/Operators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //overload the “==” operator so it checks if two employee objects are equal by comparing their id property
+            Employee e = new Employee();
+            Employee b = new Employee();
+
+            int eId = 45; //use the newly overloaded == operator to compare two employee objects by their id
+            int eId2 = 45; // change values to reflect if values are or are not equal
+
+            bool same = false; // set boolean variable same to false 
+
+            e.Id = eId;  // set e and b values in order to compare Id values
+            b.Id = eId2; 
+
+            same = e == b; // determine if e and b are equal to each other
+            Console.WriteLine(same);
+            Console.ReadLine();
+
+
+        }
+    }
+}
